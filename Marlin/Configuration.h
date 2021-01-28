4528940898f6d6a045948dd13f82748ef7edcc55
Lori <lorenzen.christian@gmail.com>
2021-01-28 18:13:12 +0100

    fast bltouch probing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1004,10 +1004,10 @@
 #define XY_PROBE_SPEED (166*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
-#define Z_PROBE_SPEED_FAST (4*60)
+#define Z_PROBE_SPEED_FAST (8*60)
 
 // Feedrate (mm/min) for the "accurate" probe of each point
-#define Z_PROBE_SPEED_SLOW (Z_PROBE_SPEED_FAST / 2)
+#define Z_PROBE_SPEED_SLOW (Z_PROBE_SPEED_FAST)
 
 /**
  * Probe Activation Switch
